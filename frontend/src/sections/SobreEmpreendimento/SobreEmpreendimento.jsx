58815ed9a8db8710c4713510d80183b0c31870fb
Gilberto Santos <gfernandesdc@gmail.com>
2026-07-16 13:50:39 -0300

    Atualiza correções do Sobre o Empreendimento

--- frontend/src/sections/SobreEmpreendimento/SobreEmpreendimento.jsx
+++ frontend/src/sections/SobreEmpreendimento/SobreEmpreendimento.jsx
@@ -11,9 +11,17 @@ function SobreEmpreendimento() {
         <h2>
           Conheça o Orla Central
         </h2>
-        <h3 className="sobre-frase">
-            O lugar que Niterói nasceu. Agora renasce para você.
-        </h3>
+        <div className="sobre-frase">
+          <p>
+            Conheça o Orla Central, um empreendimento em uma das regiões que mais se valorizam na cidade.
+          </p>
+          <p>
+            A apenas 5 minutos das Barcas, com fácil acesso à Ponte Rio–Niterói e cercado por comércio, serviços e toda a conveniência que você precisa, o Centro de Niterói se consolidou como o mais novo point de moradia da cidade. Ele une mobilidade, praticidade e qualidade de vida.
+          </p>
+          <p>
+          <strong >Solicite um Atendimento para agendar sua visita e descubra seu novo endereço!</strong>
+          </p>
+        </div>
       </div>
     </section>
   );
